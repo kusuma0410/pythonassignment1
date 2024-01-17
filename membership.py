d9e6f118d6e3b092dfa428ec747eb99d0e16976b
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-17 16:19:47 +0530

    Add files via upload

---
+++ membership.py
@@ -0,0 +1,16 @@
+#Membership Operators
+a=[10,39,20,49,20]
+print(39 in a)
+
+list=['apple','badam','orange','banana']
+print('Apple' in list)
+
+string='welcome to python world'
+print('to'in string)
+
+num=[1,3,4,5,6,7]
+print(10 not in num)
+
+name={'abc','xyz'}
+print('abc' in name)
+print('xyz'not in name)
