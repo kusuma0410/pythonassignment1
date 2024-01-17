41e05d7c2396537d55d1e9d62e354081e5050d01
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-17 16:05:42 +0530

    Add files via upload

---
+++ ternary.py
@@ -0,0 +1,16 @@
+#ternary operator
+#x=first _value if condition else second_value
+x=20 if 20>60 else 80
+print(x)#80
+
+y=60 if 40>60 else 100
+print(y)#100
+
+z=100 if 80>100 else 200
+print(z)#200
+
+a=3 if 2>3 else 5
+print(a)#5
+
+k=10 if 10<15 else 2
+print(k)
