cde8477805287c7a595ef35299dd10c0054ecb82
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-17 16:20:22 +0530

    Add files via upload

---
+++ identity.py
@@ -0,0 +1,23 @@
+#idendity Operator
+a=40
+b=30
+print(a is b)
+
+k=[19,10,38,10]
+s=[19,10,38,10]
+print(k is not s)
+
+value='abc'
+name= value
+print(value is name)
+print(value is not name)
+
+dict1={'a':1,'b':2}
+dict2={'a':1,'b':2}
+print(dict1 is dict2)
+print(dict1 is not dict2)
+
+x=[1,3,4,5]
+y=x
+print(x is not y)
+print(x is y)
