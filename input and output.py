3d95d5dbe0a7fbc8ea0593384eb161aa4285274d
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-17 16:19:23 +0530

    Add files via upload

---
+++ input and output.py
@@ -0,0 +1,20 @@
+#input
+num=int(input("enter the number"))
+num1=num+4
+print(num1)
+
+name=input("enter your name")
+print('hello',name)
+
+age=int(input("enter your age:"))
+print("you will be "+str(age+1)+"years old next year")
+
+#output
+print('abc')
+print('abc',end=' ')
+print('abc')
+print('abc') 
+
+print("hello abc" + "welcome ")
+
+print("hello abc" , "welcome ")
