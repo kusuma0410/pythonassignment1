86adae40f3c9c6a458196db1e9a7d4e817fc1a5b
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-18 12:19:44 +0530

    Add files via upload

---
+++ biggestthreenum.py
@@ -0,0 +1,20 @@
+a = 25
+b = 35
+c = 15
+largest = max(a, b, c)
+print("The largest number among", a, ",", b, "and", c, "is", largest)#35
+
+
+a = 250
+b = 350
+c = 150
+largest = max(a, b, c)
+print("The largest number among", a, ",", b, "and", c, "is", largest)#350
+
+
+a = 200
+b = 350
+c = 150
+d = 400
+largest = max(a, b, c,d)
+print("The largest number among", a, ",", b,c,",", "and", d, "is", largest)#350
