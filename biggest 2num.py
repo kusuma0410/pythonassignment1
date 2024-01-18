361e291f1180c2a8080619815ab96006701e038d
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-18 12:19:19 +0530

    Add files via upload

---
+++ biggest 2num.py
@@ -0,0 +1,31 @@
+print("Enter Three Numbers: ")
+numOne = int(input())
+numTwo = int(input())
+
+if numOne>numTwo:
+    print("\nLargest Number =", numOne)
+else:
+    print("\nLargest Number =", numTwo)
+
+
+
+a=110
+b=20
+largest=max(a,b)
+print("the largest number among" ,a,",",b,"is",largest)#110
+
+a=250
+b=290
+largest=max(a,b)
+print("the largest number among" ,a,",",b,"is",largest)#290
+
+a=330
+b=150
+largest=max(a,b)
+print("the largest number among" ,a,",",b,"is",largest)#330
+
+
+a=110
+b=220
+largest=max(a,b)
+print("the largest number among" ,a,",",b,"is",largest)#220
