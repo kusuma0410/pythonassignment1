825753c7f99982023484003fcfd3142b8bb6f0ac
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-18 16:40:39 +0530

    Add files via upload

---
+++ inbetween1to100.py
@@ -0,0 +1,37 @@
+#x = int(input("Enter a number: "))
+#if x>=1 and x<=100:
+    #print('You typed', x, "it between 1 and 100.")
+#else:
+    #print('You typed', x, "it does not between 1 and 100.")
+
+
+
+i = 1
+while i>=1 and i <= 100:
+    print(i, end=" ")
+    i += 1
+
+
+   # for i in range(1,101):
+     #print(i, end=" ")#it print multiple time(1,2,...100)
+
+
+#n = int(input('Enter a number : '))
+#i = 1
+#while i <= n:
+    #print(i, end=" ")#in this we have to give the output 10 and then it will print(1 2 3 4 5 6 7 8 9 10)
+    #i+=1
+
+#i = int(input('Enter number : '))
+#print([i for i in range(1,i+1)])#it will give same output in the above prog
+
+
+
+
+
+
+x = int(input("Enter a number: "))
+if x>=1 and x<=100:
+    print('You typed', x, "it between 1 and 100.")
+else:
+    print('You typed', x, "it does not between 1 and 100.")
