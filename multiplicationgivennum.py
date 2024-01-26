bd5083731db234aaa248188c5f41ae4132342d59
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-26 21:53:13 +0530

    Add files via upload

---
+++ multiplicationgivennum.py
@@ -0,0 +1,31 @@
+#num = 2
+#for i in range(1, 11):
+ #  print(num, 'x', i, '=', num*i)
+
+#num = 10
+#for i in range(1, 11):
+ #  print(num, 'x', i, '=', num*i)
+
+#num = 22
+#for i in range(1, 11):
+ #  print(num, 'x', i, '=', num*i)
+   
+#num = 22
+#for i in range(1, 16):
+ #  print(num, 'x', i, '=', num*i)
+
+
+ #num = 22
+#for i in range(1, 21):
+  # print(num, 'x', i, '=', num*i)
+   
+
+#num = 2
+#for i in range(1, 21):
+ #  print(num, 'x', i, '=', num*i)
+
+
+
+num = 2
+for i in range(11, -1):
+   print(num, 'x', -i, '=', num*-i)
