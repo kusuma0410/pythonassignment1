ccbb287624834d812627ed41c63e6574134e3b23
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-26 20:06:01 +0530

    Add files via upload

---
+++ sumofallnum.py
@@ -0,0 +1,12 @@
+number = 10
+print(sum(range(number+1)))
+
+numbers = [1, 2, 3, 4,5,6,7,8,9,10]
+print(sum(numbers))
+
+
+number = int(input("Number: "))
+sum = 0
+for i in range(1, number + 1):
+    sum += i
+print(sum)
