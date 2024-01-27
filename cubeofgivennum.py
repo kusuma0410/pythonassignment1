584ff334e3195642908f7d8a22fad5cadfc5cb7e
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-27 23:20:30 +0530

    Add files via upload

---
+++ cubeofgivennum.py
@@ -0,0 +1,12 @@
+#number = float(input(" Please Enter any numeric Value : "))
+#cube = number ** 3
+#print("The Cube of a Given Number {0}  = {1}".format(number, cube))
+
+
+def cube(num):
+    return num * num * num
+number = float(input(" Please Enter any numeric Value : "))
+cub = cube(number)
+print("The Cube of a Given Number {0}  = {1}".format(number, cub))
+
+
