ca5ab536a6805ce524a90f42ebd8890620371293
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-27 23:45:05 +0530

    Update blocktodisplaydone.py

--- blocktodisplaydone.py
+++ blocktodisplaydone.py
@@ -1,39 +1,41 @@
-#for i in range(10):
- #   print(i)
+#example:
+for i in range(10):
+    print(i)
 
+#case 1:
+for i in range(5):
+    print(i)
+else:
+    print("Done!")
 
-#for i in range(5):
- #   print(i)
-#else:
- #   print("Done!")
+ #case 1a:
+for i in range(10):
+    print(i)
+else:
+    print("Done!")
+ 
+#case 1b:
+for i in range(17):
+    print(i)
+else:
+   print("Done!")
+#case 1c: 
+for i in range(10,20): 
+	print(i,end=' ') 
+else: 
+	print('Done') #o/p:10 11 12 13 14 15 16 17 18 19 Done
 
-
-#for i in range(10):
-   # print(i)
-#else:
- #   print("Done!")
-
-#for i in range(17):
- #   print(i)
-#else:
- #  print("Done!")
-    #case 1: 
-#for i in range(10,20): 
-#	print(i,end=' ') 
-#else: 
-#	print('Done') #o/p:10 11 12 13 14 15 16 17 18 19 Done
-
-
+#case 1d: 
 for i in range(1,10): 
 	print(i,end=' ') 
 else: 
 	print('Done')
 	
-
-#for i in range(10,30): 
-#	print(i,end=' ') 
-#else: 
-#	print('Done')
+case 1e: 
+for i in range(10,30): 
+	print(i,end=' ') 
+else: 
+	print('Done')
 
 for i in range(30,40): 
 	print(i,end=' ') 
@@ -42,14 +44,14 @@ else:
     
 
 #case 2: 
-#for i in range(20,10): 
-#	print(i,end=' ') 
-#else: 
-	#print('Done') #o/p:Done
-	
-#for i in range(10,5): 
-#	print(i,end=' ') 
-#else: 
-	#print('Done')#o/p:Done   
+for i in range(20,10): 
+	print(i,end=' ') 
+else: 
+	print('Done') #o/p:Done
+ #case 2a:	
+for i in range(10,5): 
+	print(i,end=' ') 
+else: 
+	print('Done')#o/p:Done   
 
 
