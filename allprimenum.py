b653197b235c395fa8cdeeaa211db36b138bfe61
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-27 23:48:33 +0530

    Update allprimenum.py

--- allprimenum.py
+++ allprimenum.py
@@ -1,17 +1,16 @@
-# Python program to display all the prime numbers within an interval
-
-#lower = 0
-#upper = 20
-#print("Prime numbers between", lower, "and", upper, "are:")
-#for num in range(lower, upper + 1):
-   # all prime numbers are greater than 1
- #  if num > 1:
-  #     for i in range(2, num):
-   #        if (num % i) == 0:
-    #           break
-     #  else:
-      #     print(num)
-
+#example:
+lower = 0
+upper = 20
+print("Prime numbers between", lower, "and", upper, "are:")
+for num in range(lower, upper + 1):
+    all prime numbers are greater than 1
+  if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+#eg1:
 k = 0
 s = 0
 print("Prime numbers between", k, "and", s, "are:")
@@ -24,21 +23,13 @@ for num in range(k, s + 1):
        else:
            print(num)
 
-
-
-
-
-
-
-
-
-#start = 25
-#end = 50
-
-#for num in range(start, end + 1):
- #   if num > 1: # all prime #s are greater than 1
- #       for i in range(2,num):
-  #          if (num % i) == 0:
-   #             break
-    #    else:
-     #       print(num)       
+#eg2:
+start = 25
+end = 50
+for num in range(start, end + 1):
+    if num > 1: # all prime #s are greater than 1
+        for i in range(2,num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)       
