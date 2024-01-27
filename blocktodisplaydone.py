238c1b809847b87f8d43d6a58b23ad57ce558e9b
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-27 23:07:47 +0530

    Add files via upload

---
+++ blocktodisplaydone.py
@@ -0,0 +1,55 @@
+#for i in range(10):
+ #   print(i)
+
+
+#for i in range(5):
+ #   print(i)
+#else:
+ #   print("Done!")
+
+
+#for i in range(10):
+   # print(i)
+#else:
+ #   print("Done!")
+
+#for i in range(17):
+ #   print(i)
+#else:
+ #  print("Done!")
+    #case 1: 
+#for i in range(10,20): 
+#	print(i,end=' ') 
+#else: 
+#	print('Done') #o/p:10 11 12 13 14 15 16 17 18 19 Done
+
+
+for i in range(1,10): 
+	print(i,end=' ') 
+else: 
+	print('Done')
+	
+
+#for i in range(10,30): 
+#	print(i,end=' ') 
+#else: 
+#	print('Done')
+
+for i in range(30,40): 
+	print(i,end=' ') 
+else: 
+	print('Done')
+    
+
+#case 2: 
+#for i in range(20,10): 
+#	print(i,end=' ') 
+#else: 
+	#print('Done') #o/p:Done
+	
+#for i in range(10,5): 
+#	print(i,end=' ') 
+#else: 
+	#print('Done')#o/p:Done   
+
+
