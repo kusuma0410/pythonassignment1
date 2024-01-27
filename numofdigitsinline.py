e2644f989a3875efd5461b18605531d05854c393
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-27 23:22:20 +0530

    Add files via upload

---
+++ numofdigitsinline.py
@@ -0,0 +1,50 @@
+#num = 123456
+#print(len(str(num)))#o/p:6
+
+#num = 123456789
+#print(len(str(num)))#o/p:9
+
+
+#num = 1,2,3,4,5,6,7,8,9,1011,12,13,14
+#print(len(str(num)))#o/p:45
+
+
+#num = 123456
+#count = 0
+#while num != 0:
+ #   num //= 10
+  #  count += 1
+#print("Number of digits: " + str(count))#o/p:6
+
+#num = 1234567891011
+#count = 0
+#while num != 0:
+    #num //= 10
+    #count += 1
+#print("Number of digits: " + str(count))#o/p:13
+
+
+
+
+
+
+
+
+#k = 'Python'
+#for x in k:
+ #   print(x)#o/p:P
+
+
+   # iterate from i = 0 to i = 3
+for i in range(4):
+    print(i)
+
+k = ['abc', 'xyz', '123']
+for i in k:
+    print(i)
+
+
+
+
+# access items of a list using for loop
+
