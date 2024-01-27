6a2eccb3319ba69d3ef2fcb0a2a8816a5eb0a175
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-27 23:37:16 +0530

    Update numofdigitsinline.py

--- numofdigitsinline.py
+++ numofdigitsinline.py
@@ -1,27 +1,29 @@
-#num = 123456
-#print(len(str(num)))#o/p:6
+#example:
+num = 123456
+print(len(str(num)))#o/p:6
+#eg1:
+num = 123456789
+print(len(str(num)))#o/p:9
 
-#num = 123456789
-#print(len(str(num)))#o/p:9
+#eg2:
+num = 1,2,3,4,5,6,7,8,9,1011,12,13,14
+print(len(str(num)))#o/p:45
 
+#eg3:
+num = 123456
+count = 0
+while num != 0:
+    num //= 10
+    count += 1
+print("Number of digits: " + str(count))#o/p:6
 
-#num = 1,2,3,4,5,6,7,8,9,1011,12,13,14
-#print(len(str(num)))#o/p:45
-
-
-#num = 123456
-#count = 0
-#while num != 0:
- #   num //= 10
-  #  count += 1
-#print("Number of digits: " + str(count))#o/p:6
-
-#num = 1234567891011
-#count = 0
-#while num != 0:
-    #num //= 10
-    #count += 1
-#print("Number of digits: " + str(count))#o/p:13
+#eg4:
+num = 1234567891011
+count = 0
+while num != 0:
+    num //= 10
+    count += 1
+print("Number of digits: " + str(count))#o/p:13
 
 
 
