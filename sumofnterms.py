3fbb9e024891fad223555966a1197e45304786a5
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-27 23:23:18 +0530

    Add files via upload

---
+++ sumofnterms.py
@@ -0,0 +1,50 @@
+
+
+
+
+#sum = 0
+#i=0
+#print(end="Enter the Value of n: ")
+#n = int(input())
+#print(end="Enter " + str(n) + " Numbers: ")
+#while i<n:
+ #   num = int(input())
+  #  sum = sum+num
+   # i = i+1
+#print("\nSum of " + str(n) + " Numbers = " + str(sum))
+
+
+
+
+num = [1,2,3]
+sum = 0
+print(end="Enter the Value of n: ")
+n = int(input())
+
+print(end="Enter " + str(n) + " Numbers: ")
+for i in range(n):
+    num.insert(i, int(input()))
+
+for i in range(n):
+    sum = sum+num[i]
+
+print("\nSum of " + str(n) + " Numbers = " + str(sum))
+
+
+#num = []
+##print(end="Enter the Value of n: ")
+#n = int(input())
+
+#print(end="Enter " + str(n) + " Numbers: ")
+#for i in range(n):
+ #   num.insert(i, int(input()))
+
+#for i in range(n):
+ #   sum = sum+num[i]
+
+#print("\nSum of " + str(n) + " Numbers = " + str(sum))
+
+
+
+
+
