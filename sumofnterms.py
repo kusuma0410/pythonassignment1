9b68e53c909fcbff51e54dd7e83a6be91951e944
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-28 15:07:41 +0530

    Update sumofnterms.py

--- sumofnterms.py
+++ sumofnterms.py
@@ -1,50 +1,33 @@
+#Using a for loop
+sum = 0
+print("Enter the Value of n: ")
+n = int(input())
+print("Enter " + str(n) + " Numbers: ")
+for i in range(n):
+    num = int(input())
+    sum = sum+num
+print("Sum of " + str(n) + " Numbers = " + str(sum))
 
+#Using a while loop
+sum = 0
+i=0
+print(end="Enter the Value of n: ")
+n = int(input())
+print(end="Enter " + str(n) + " Numbers: ")
+while i<n:
+    num = int(input())
+    sum = sum+num
+    i = i+1
+print("\nSum of " + str(n) + " Numbers = " + str(sum))
 
-
-
-#sum = 0
-#i=0
-#print(end="Enter the Value of n: ")
-#n = int(input())
-#print(end="Enter " + str(n) + " Numbers: ")
-#while i<n:
- #   num = int(input())
-  #  sum = sum+num
-   # i = i+1
-#print("\nSum of " + str(n) + " Numbers = " + str(sum))
-
-
-
-
-num = [1,2,3]
+#using a list
+num = []
 sum = 0
 print(end="Enter the Value of n: ")
 n = int(input())
-
 print(end="Enter " + str(n) + " Numbers: ")
 for i in range(n):
     num.insert(i, int(input()))
-
 for i in range(n):
     sum = sum+num[i]
-
 print("\nSum of " + str(n) + " Numbers = " + str(sum))
-
-
-#num = []
-##print(end="Enter the Value of n: ")
-#n = int(input())
-
-#print(end="Enter " + str(n) + " Numbers: ")
-#for i in range(n):
- #   num.insert(i, int(input()))
-
-#for i in range(n):
- #   sum = sum+num[i]
-
-#print("\nSum of " + str(n) + " Numbers = " + str(sum))
-
-
-
-
-
