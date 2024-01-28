9690c57158480b86f9cbf4a4c5856672b3c64cef
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-28 14:50:59 +0530

    Add files via upload

---
+++ looplist.py
@@ -0,0 +1,26 @@
+#a= list["apple", "banana", "grapes"] 
+#print(a)#o/p:list['apple', 'banana', 'grapes']
+
+#k=["abc", "xyz", "123"]
+#for x in k:
+  #print(x)
+
+
+#k= list["abc", "xyz", "123"]
+#for x in k:
+ # print(x)#o/p:list['abc', 'xyz', '123']
+
+s= ["apple", "ball", "cat","dog"]
+for i in range(len(s)):
+  print(s[i])
+
+y= ["ant", "ball", "cat"]
+[print(x) for x in y]
+
+
+#while
+z= ["abc", "123", "xyz","1100"]
+i = 0
+while i < len(z):
+  print(z[i])
+  i = i + 1
