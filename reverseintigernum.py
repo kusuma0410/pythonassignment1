4b8700d9d875c2d1620d9c967bec9def623e186b
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-28 15:16:04 +0530

    Update reverseintigernum.py

--- reverseintigernum.py
+++ reverseintigernum.py
@@ -1,20 +1,17 @@
 a = 1534236469
-print(int(str(a)[::-1]))
+print(int(str(a)[::-1]))#o/p:9646324351
 
+b = 123456789
+print(int(str(b)[::-1]))#o/p:98765432
 
-a = 123456789
-print(int(str(a)[::-1]))
+x = 54321
+print(int(str(x)[::-1]))#o/p:12345
 
-a = ("abc")
-print(str(a)[::-1])
+k = ("xyz")
+print(str(k)[::-1])#o/p:zyx
 
+k1 = ("zyx")
+print(str(k1)[::-1])#o/p:xyz
 
-
-
-
-
-
-
-
-
-
+s = ("abcdefgh")
+print(str(s)[::-1])#o/p:hgfedcba
