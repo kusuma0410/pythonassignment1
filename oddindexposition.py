1bd2c18143cfed67d1b4bddc056722003e80642d
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-01-28 14:52:33 +0530

    Add files via upload

---
+++ oddindexposition.py
@@ -0,0 +1,33 @@
+#arr = [1, 2, 3, 4, 5]
+#Loop through the array by incrementing the value of i by 2.
+#for i in range(0, len(arr), 2):
+  #print(arr[i])
+
+#k = [31, 42, 13, 34, 85, 0, 99, 1, 3]
+#print("The list is :")
+#print(k)
+#print("The elements in odd positions are : ")
+#for i in range(1, len(k), 2):
+  # print(k[i])
+
+#s = [1,2,3,4,5,6,7,8,9,]
+#print("The list is :")
+#print(s)
+#print("The elements in odd positions are : ")
+#for i in range(2, len(s), 2):
+  # print(s[i])
+
+x = [11,14,17,21,36,49,5,2]
+print("The list is :")
+print(x)
+print("The elements in odd positions are : ")
+for i in range(1, len(x), 2):
+   print(x[i])
+
+
+y = [5,26,29,17,13,47,33,38,49]
+print("The list is :")
+print(y)
+print("The elements in odd positions are : ")
+for i in range(2, len(y), 2):
+   print(y[i])
