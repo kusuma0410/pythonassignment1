96cb9e1d3b9160c7386e0676a8027c64a443498d
kusuma0410 <153718408+kusuma0410@users.noreply.github.com>
2024-02-07 15:14:39 +0530

    Add files via upload

---
+++ assignmet3list.py
@@ -0,0 +1,88 @@
+#len(list)
+#count() 
+#index()
+##count()
+#k= ['apple', 'banana', 'cherry']
+#s= k.count("cherry")
+#print(s)
+
+#k1= [1, 4, 2, 9, 7, 8, 9, 3, 1]
+#x = k1.count(9)
+#print(x)
+
+##index()
+#l=[10,20,30,40,50]
+#print(l)
+#print(l.index(1))
+
+#k = ['apple', 'banana', 'cherry']
+#x = k.index("cherry")
+#print(x)
+
+#k1 = [4, 55, 64, 32, 16, 32]
+#x = k1.index(32)
+#print(x)
+
+# # append()
+#l=[]
+#l.append(10)
+#l.append(20)
+#l.append(30)
+#l.append(20)
+#l.append(40)
+#l.append(50)
+#print(l)
+#k=[]
+#k.append("apple")
+#print(k)
+
+#l=[10,20,30,40,50,60,70,80,20]
+#l.insert(4,888)
+#l.insert(3,111)
+#l.insert(4,888)
+#l.insert(7,100)
+#l.insert(7,888)
+#print(l)
+
+
+# #extend()
+#l1=["beer","whisky","rum"]
+#l2=["chicken","fish","prawns"]
+#l1.extend("aman raju")
+#print(l1)
+#print(l2)
+
+#k= ['apple', 'banana', 'cherry']
+#p= (1, 4, 5, 9)
+#k.extend(p)
+#print(k)
+#print(p)
+
+#l1=[10,20,30,40,50]
+#l1.remove(20)
+#print(l1)
+#k=["apple","banana","cherry"]
+#k.remove("banana")
+#print(k)
+
+#k=["red","green","blue","yellow"]
+#k.remove("blue")
+#print(k)
+
+# #pop()
+#l1=[10,20,30,40,50]
+#print(l1.pop(1))
+
+#k=["apple","banana","cherry"]
+#k.pop(1)
+#print(k)
+
+#k=["red","green","blue","yellow"]
+#k.pop(2)
+#print(k)
+
+l=[10,20,[30,40],50,60]
+print(l[2])
+
+l=[10,20,[30,40],[50,60]]
+print(l[3])
